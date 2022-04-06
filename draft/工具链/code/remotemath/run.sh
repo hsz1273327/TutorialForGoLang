04d522c9f1657c9ca409e6fee292cc4ef966e580
HUANG SIZHE <hsz1273327@gmail.com>
2022-04-06 16:33:18 +0800

    Update run.sh

--- draft/工具链/code/remotemath/run.sh
+++ draft/工具链/code/remotemath/run.sh
@@ -1,6 +1,6 @@
 export GOPRIVATE="dev.hszofficial.site" && go run main.go
 
-
+export GOVCS=dev.hszofficial.site:git,*:git
 
 [url "ssh://git@dev.hszofficial.site:9022"]
         insteadOf = ssh://git@dev.hszofficial.site
